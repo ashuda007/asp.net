6e6547802218ba3b341c95b2e62e2d9a7a64e29f
ashuda007 <112532884+ashuda007@users.noreply.github.com>
2023-09-03 12:59:06 +0530

    ok

---
+++ 7 Registrationform/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string m;
+    string c;
+  
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+        if (CheckBox1.Checked)
+        {
+            TextBox3.Text = TextBox2.Text;
+        }
+        else
+        {
+            TextBox3.Text="";
+        }
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+          if (RadioButton1.Checked)
+        {
+             m=RadioButton1.Text;
+        }
+        else if (RadioButton2.Checked)
+        {
+            m= RadioButton2.Text;
+        }
+
+         
+          if (CheckBox2.Checked)
+          {
+              c = CheckBox2.Text;
+          }
+          if (CheckBox3.Checked)
+          {
+
+              c= c+ " , " + CheckBox3.Text;
+          }
+          if (CheckBox4.Checked)
+          {
+
+              c = c + " , " + CheckBox4.Text;
+          }
+
+        Response.Redirect("Uploaddetail.aspx?fn=" + TextBox1.Text + "&padd=" + TextBox2.Text + "&ctr=" + DropDownList1.SelectedItem + "&sta=" + DropDownList2.Text + "&ct=" + DropDownList3.SelectedItem + "&gd=" + m +"&hb="+c );
+    }
+  
+}
---
+++ 7 Registrationform/show.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class show : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+      //  Image1.ImageUrl = Request.QueryString["img"];
+        TextBox1.Text = Request.QueryString["fn"];
+        TextBox2.Text = Request.QueryString["padd"];
+        TextBox3.Text = Request.QueryString["ctr"];
+        TextBox4.Text = Request.QueryString["sta"];
+        TextBox5.Text = Request.QueryString["ct"];
+        TextBox6.Text = Request.QueryString["gd"];
+        TextBox7.Text = Request.QueryString["hb"];
+
+       Label12.Text= Request.QueryString["ug"];
+       Label15.Text = Request.QueryString["pg"];
+       Label10.Text = Request.QueryString["twp"];
+       Label13.Text = Request.QueryString["ugp"];
+       Label16.Text = Request.QueryString["pgp"];
+       Image1.ImageUrl = Request.QueryString["fi"];
+       Image2.ImageUrl = Request.QueryString["fi2"];
+       HyperLink1.NavigateUrl = Request.QueryString["fp"];
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
